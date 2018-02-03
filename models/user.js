9d7650df2aef4951d3cfdcbd6083cc426c188312
lipengzhou <lpzmail@163.com>
2018-02-03 15:45:14 +0800

    添加业务模型

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const {query} = require('../utilities/db-helper')
+
+exports.findByEmail = (email, callback) => {
+  query('SELECT * FROM `users` WHERE `email`=?', [email], (err, results) => {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, results[0])
+  })
+}
+
+exports.findByNicknam = (nickname, callback) => {
+  query('SELECT * FROM `users` WHERE `nickname`=?', [nickname], (err, results) => {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, results[0])
+  })
+}
+
+exports.save = (user, callback) => {
+  const sqlStr = 'INSERT INTO `users` SET ?'
+  query(sqlStr, user, (err, results) => {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, results)
+  })
+}
