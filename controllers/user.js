a78b8fad3a5e7c5fc76c32702a07dcf2abeebade
lipengzhou <lpzmail@163.com>
2018-02-03 16:25:41 +0800

    完成基本的用户注册

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,81 @@
-exports.showSignin = (req,res) => {
+const user = require('../models/user')
+const md5 = require('blueimp-md5')
+
+exports.showSignin = (req, res) => {
   res.render('signin.html')
 }
 
-exports.signin = (req,res) => {
-  res.send('post signin')
+exports.signin = (req, res) => {
+  res.send(JSON.stringify({
+    foo: 'bar'
+  }))
 }
 
-exports.showSignup = (req,res) => {
+exports.showSignup = (req, res) => {
   res.render('signup.html')
 }
 
-exports.signup = (req,res) => {
-  res.send('post signup')
+exports.signup = (req, res) => {
+  // 1. 接收表单提交的数据
+  //    配置 body-parser 中间件解析表单 POST 请求体
+  // 2. 验证数据的有效性
+  //    普通数据校验
+  //    业务数据校验
+  // 3. 验证通过，持久化保存到数据库中
+  // 4. 发送响应
+
+  const body = req.body
+
+  user.findByEmail(body.email, (err, ret) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message // err 错误对象有一个 message 属性是具体的错误消息
+      })
+    }
+
+    if (ret) {
+      return res.status(200).json({
+        code: 1,
+        message: 'email exists'
+      })
+    }
+
+    user.findByNickname(body.nickname, (err, ret) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.message // err 错误对象有一个 message 属性是具体的错误消息
+        })
+      }
+
+      if (ret) {
+        return res.status(200).json({
+          code: 2,
+          message: 'nickname exists'
+        })
+      }
+
+      body.password = md5(body.password)
+
+      user.save(body, (err, results) => {
+        if (err) {
+          return res.status(500).json({
+            error: err.message // err 错误对象有一个 message 属性是具体的错误消息
+          })
+        }
+        res.status(200).json({
+          code: 0,
+          message: 'success'
+        })
+      })
+    })
+
+    // res.status(200).json({
+    //   code: 0,
+    //   message: '注册成功'
+    // })
+  })
 }
 
-exports.signout = (req,res) => {
+exports.signout = (req, res) => {
   res.send('post signout')
 }
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ exports.findByEmail = (email, callback) => {
   })
 }
 
-exports.findByNicknam = (nickname, callback) => {
+exports.findByNickname = (nickname, callback) => {
   query('SELECT * FROM `users` WHERE `nickname`=?', [nickname], (err, results) => {
     if (err) {
       return callback(err)
@@ -19,6 +19,7 @@ exports.findByNicknam = (nickname, callback) => {
 }
 
 exports.save = (user, callback) => {
+  user.createdAt = null // 创建时间给一个 null ，数据库会自动生成当前时间戳
   const sqlStr = 'INSERT INTO `users` SET ?'
   query(sqlStr, user, (err, results) => {
     if (err) {
