58c9ca57748e335e11b54b5c3b5e330a0d516d0f
lipengzhou <lpzmail@163.com>
2018-02-03 11:46:26 +0800

    按照功能业务提取划分控制器模块

--- router.js
+++ router.js
@@ -3,25 +3,38 @@
 // 0. 加载 express
 const express = require('express')
 
+const userController = require('./controllers/user')
+const topicController = require('./controllers/topic')
+const commentController = require('./controllers/comment')
+const indexController = require('./controllers/index')
+
 // 1. 创建路由创建
 const router = express.Router()
 
 // 2. 配置路由表
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-router.get('/signin', (req, res) => {
-  res.send('signin')
-})
+// 首页相关
+router
+  .get('/', indexController.showIndex)
 
-router.get('/signup', (req, res) => {
-  res.send('signup')
-})
+// 用户相关
+router
+  .get('/signin', userController.showSignin)
+  .post('/signin', userController.signin)
+  .get('/signup', userController.showSignup)
+  .post('/signup', userController.signup)
+  .post('/signout', userController.signout)
 
-router.get('/signout', (req, res) => {
-  res.send('signout')
-})
+// 话题相关
+router
+  .get('/topic/create', topicController.showCreate)
+  .post('/topic/create', topicController.create)
+  .get('/topic/:topicId', topicController.showDetail)
+  .get('/topic/:topicID/edit', topicController.showEdit)
+  .post('/topic/:topicID/edit', topicController.edit)
+  .post('/topic/:topicID/delete', topicController.delete)
+
+// 评论相关
 
 // 3. 导出路由容器
 // 4. app.js
