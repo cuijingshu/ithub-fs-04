4342ff3f6c5a15a9efa370f5ef4f88f58746a3c4
lipengzhou <lpzmail@163.com>
2018-02-05 17:22:16 +0800

    处理完成发布话题功能

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,10 +1,37 @@
+const topic = require('../models/topic')
+const moment = require('moment')
+
 exports.showCreate = (req, res, next) => {
-  res.render('topic/create.html', {
+  topic.findAll((err, topics) => {
+    if (err) {
+      return next(err)
+    }
+    res.render('topic/create.html', {
+      topics
+    })
   })
 }
 
 exports.create = (req, res, next) => {
-  res.send('post create')
+  // 1. 获取表单提交的数据
+  // 2. 数据验证
+  // 3. 操作数据库
+  // 4. 发送响应
+  const topicData = {
+    ...req.body,
+    createdAt: moment().format('YYYY-MM-DD HH:m:ss'),
+    userId: req.session.user.id
+  }
+
+  topic.save(topicData, (err, results) => {
+    if (err) {
+      return next(err)
+    }
+    res.status(200).json({
+      code: 0,
+      message: 'success'
+    })
+  })
 }
 
 exports.showDetail = (req, res, next) => {
