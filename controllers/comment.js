5b2764881a0265833c97e474fce914aa6edd9b7a
lipengzhou <lpzmail@163.com>
2018-02-08 15:00:19 +0800

    设计评论路由及评论控制器

---
+++ controllers/comment.js
@@ -0,0 +1,19 @@
+exports.list = (req, res, next) => {
+  res.send('get list')
+}
+
+exports.create = (req, res, next) => {
+  res.send('post create')
+}
+
+exports.showEdit = (req, res, next) => {
+  res.send('get showEdit')
+}
+
+exports.edit = (req, res, next) => {
+  res.send('post edit')
+}
+
+exports.delete = (req, res, next) => {
+  res.send('post delete')
+}
