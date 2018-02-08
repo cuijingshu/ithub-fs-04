fea93d11fae82ecc142c6a1c09dcdb500350dcad
lipengzhou <lpzmail@163.com>
2018-02-08 16:08:48 +0800

    根据同步和异步请求做不同的校验登陆状态处理

--- routes/comment.js
+++ routes/comment.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const commentController = require('../controllers/comment')
+const {checkLogin} = require('../middlewares/auth')
 
 const router = express.Router()
 
 router
   .get('/:topicId/comment', commentController.list) // 获取话题评论列表
-  .post('/:topicId/comment', commentController.create) // 发表评论
+  .post('/:topicId/comment', checkLogin, commentController.create) // 发表评论
   .get('/:topicId/comment/:commentId/edit', commentController.showEdit) // 获取编辑评论
   .post('/:topicId/comment/:commentId/edit', commentController.edit) // 处理编辑评论
   .post('/:topicId/comment/:commentId/delete', commentController.delete) // 删除评论
